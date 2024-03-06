6527ee8023579b3f703362e1e2361e3967ac7a5c
ritik <ritikbhatt020@gmail.com>
2024-03-06 17:57:41 +0530

    🚀Registering users done

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -35,3 +37,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been Logged Out...")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have Successfully Registered...Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})   
