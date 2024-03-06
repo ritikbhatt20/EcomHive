68d1b0f48acb79d5d6d64c7fd7a854c4747ff7cb
ritik <ritikbhatt020@gmail.com>
2024-03-06 16:38:25 +0530

    🏺Completed login logout fn

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,11 +9,29 @@ def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
 
+
 def about(request):
     return render(request, 'about.html', {})
 
+
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an Error Loggin in, please try again...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+    
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been Logged Out...")
+    return redirect('home')
