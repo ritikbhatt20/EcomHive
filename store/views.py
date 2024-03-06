6e3ea7db6569c0bdf7bc7ab3ddeb0679508f7dcf
ritik <ritikbhatt020@gmail.com>
2024-03-06 15:51:12 +0530

    ⚙️Basic Template for login logout

--- store/views.py
+++ store/views.py
@@ -9,3 +9,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    pass
