9684cc70968a708b4d61f332f0cd1bcf184c7f31
ritik <ritikbhatt020@gmail.com>
2024-03-15 02:36:02 +0530

    🚀Made changes

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),     
     path('product/<int:pk>', views.product, name='product'),            
     path('category/<str:foo>', views.category, name='category'),                                                                          
-]
+]  
